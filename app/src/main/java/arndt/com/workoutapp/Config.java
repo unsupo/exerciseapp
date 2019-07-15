6ea0f1614c733de220cc4ca92e60baf3af373673
Jonathan <unsupo@gmail.com>
2019-07-15 12:19:28 -0600

    Update Config.java

--- app/src/main/java/arndt/com/workoutapp/Config.java
+++ app/src/main/java/arndt/com/workoutapp/Config.java
@@ -1,5 +1,5 @@
 package arndt.com.workoutapp;
 
 public class Config {
-    public static final String YOUTUBE_API_KEY = "AIzaSyDZje_Rhf-wQepQgUrWg6ctYGed8qxLZ2U";
+    public static final String YOUTUBE_API_KEY = "";
 }
